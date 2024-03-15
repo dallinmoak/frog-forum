e86fecfe9011c7f12c4a5c313f5fd155138a1000
dallinmoak <dallinmoak@gmail.com>
2024-03-15 17:41:26 -0600

    add very basic routing

--- src/App.svelte
+++ src/App.svelte
@@ -1,3 +1,42 @@
+<script>
+  import { Router, Route, Link } from "svelte-routing";
+
+  let currentUserId = 1;
+</script>
+
 <main>
-  <h1>Hello, World!</h1>
+  <Router>
+    <header>
+      <h2>Frog Forum</h2>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home/feed</Link>
+          </li>
+          <li>
+            <Link to="/signup">Sign up</Link>
+          </li>
+          <li>
+            <Link to="/signin">Sign in</Link>
+          </li>
+          <li>
+            <Link to="/profile/{currentUserId}">My Profile</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <Route path="/">
+      <h1>Home/feed page</h1>
+    </Route>
+    <Route path="/signup">
+      <h1>Sign up page</h1>
+    </Route>
+    <Route path="/signin">
+      <h1>Sign in page</h1>
+    </Route>
+    <Route path="/profile/:userId" let:params>
+      <h1>User profile page</h1>
+      <p>User ID: {params.userId}</p>
+    </Route>
+  </Router>
 </main>

94e51c476662749dd7cda8999fc8698292b556c9
dallinmoak <dallinmoak@gmail.com>
2024-03-26 12:00:55 -0600

    add default unauthorized home view

--- src/components/pages/Home.svelte
+++ src/components/pages/Home.svelte
@@ -1,10 +1,11 @@
 <script>
   import { currentAuthStatus } from "../../stores";
   import Authed from "../home/Authed.svelte";
+  import Unauthed from "../home/Unauthed.svelte";
 </script>
 
 {#if $currentAuthStatus}
   <Authed />
 {:else}
-  <p>unathed view here</p>
+  <Unauthed />
 {/if}

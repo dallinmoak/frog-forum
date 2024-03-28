7ffe134305840778072555e5f9147ea4fed9aad2
Nicholas Keen <nicknack2019@yahoo.com>
2024-03-28 15:30:06 -0600

    made changes

--- src/components/pages/Post.svelte
+++ src/components/pages/Post.svelte
@@ -1,18 +1,23 @@
 <script>
   import { getPostData, userById } from "../../int/request";
-
+  import {onMount} from "svelte";
   export let postId;
 
   let post;
 
   const getPosts = async (id) => {
     post = await getPostData(id);
-    post.authorName = await userById(post.author).name;
+    post.authorName = (await userById(post.author)).name;
   };
+  onMount(getPosts)
+
 </script>
 
-<h1>
-  your post is id {postId}
-  <img src={post.pic} alt={post.caption} />
-  <p>{post.caption}</p>
-</h1>
+{#if post}
+  <h1>
+    your post is id {postId}
+    <img src={post.pic} alt={post.caption} />
+    <p>{post.caption}</p>
+  </h1>
+{/if}
+
